93b6025253cc4ae9846f6e4f11569d4140b7b00c
Sandro.dev <sandrosuarezaranda@gmail.com>
2024-07-18 10:10:40 +0200

    CSS variables created and implemented. New ts file created in order to manage the background image of the different pages

---
+++ src/lib/headerFunctions.ts
@@ -0,0 +1,37 @@
+
+export const updateBackgroundImage = () => {
+const infoContainer = document.querySelector('.info-container') as HTMLElement;
+    const currentUrl:string = window.location.href;
+
+    if (currentUrl.includes('body-piercing')) {
+      infoContainer?.classList.add('body-background');
+      return;
+    }
+
+    if (currentUrl.includes('ear-piercing')) {
+      infoContainer?.classList.add('ear-background');
+      return;
+    }
+
+    if (currentUrl.includes('face-piercing')) {
+      infoContainer?.classList.add('face-background');
+      return;
+    }
+      infoContainer?.classList.add('genital-background');
+}
+
+export const updateRightPosition = () => {
+    const screenWidth: number = window.innerWidth;
+    const info = document.querySelector('.info') as HTMLElement;
+
+    if (screenWidth > 1800) {
+      const extraWidth: number = screenWidth - 1800;
+      const leftPercentage: number = 10 + extraWidth / 100;
+      info.style.left = `${leftPercentage}%`;
+    }
+    if (screenWidth > 4900) {
+      const extraWidth: number = screenWidth - 5000;
+      const leftPercentage: number = 35 + extraWidth / 100;
+      info.style.left = `${leftPercentage}%`;
+    }
+  };
